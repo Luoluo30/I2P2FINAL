75bab74d730dcc1da87d2276adb8808f0a8ada3f
watermelon <117496117+kuoannabel@users.noreply.github.com>
2024-11-27 15:38:30 +0800

    Update Fruit.cpp

--- Fruit.cpp
+++ Fruit.cpp
@@ -1,16 +1,26 @@
-#include "Wall.h"
+#include "Fruit.h"
 #include "data/DataCenter.h"
 #include "data/ImageCenter.h"
 #include "shapes/Point.h"
 #include <allegro5/bitmap_draw.h>
 
-Wall::Wall(const Point &p) {
-}
+void Fruit::init() {
 
-void Wall::update() {
+    
+    ImageCenter *IC = ImageCenter::get_instance();
+    bitmap = IC->get("./assets/image/Fruit.png");
+    constexpr char  fruit_img_path[] = "./assets/image/Fruit.png";
+    bitmap = IC->get(fruit_img_path);
 
+    
+  
+   
 }
 
 void
-Wall::draw() {
+Fruit::draw() {
+	al_draw_bitmap(
+		bitmap,
+		shape->center_x() - al_get_bitmap_width(bitmap)/2,
+		shape->center_y() - al_get_bitmap_height(bitmap)/2, 0);
 }
