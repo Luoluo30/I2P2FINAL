d26dd0d1a3453988028c9248128ff1e14d1df92c
watermelon <117496117+kuoannabel@users.noreply.github.com>
2024-12-04 21:51:39 +0800

    Update Fruit.h

--- Fruit.h
+++ Fruit.h
@@ -1,18 +1,32 @@
-#ifndef PLAYER_H_INCLUDED
-#define PLAYER_H_INCLUDED
+#ifndef FRUIT_H_INCLUDED
+#define FRUIT_H_INCLUDED
+#include <string>
+#include <map>
+#include "Object.h"
+#include "Wall.h"
 
-class Player
+enum class FruitState
 {
-public:
-	Player();
-	void update();
-	int HP;
-	int coin;
-	int fruit = 0;
-private:
-	int coin_freq;
-	int coin_increase;
-	int coin_counter;
+    LEFT,
+    RIGHT,
+    FRONT,
+    BACK,
+    CHARACTERSTATE_MAX
 };
 
+class Fruit : public Object
+{
+
+public:
+    void init();
+    void update();
+    void draw();
+    
+
+private:
+    FruitState state = FruitState::FRONT; 
+    int width, height;                 
+    std::map<FruitState, std::string> gifPath;
+    
+};
 #endif
