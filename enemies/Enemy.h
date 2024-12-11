0c98207eeef0756335b56f24936127c75e5411e6
Luoluo30 <vincent921130@gmail.com>
2024-12-12 00:33:45 +0800

    更

--- enemies/Enemy.h
+++ enemies/Enemy.h
@@ -1,73 +1,73 @@
-#ifndef ENEMY_H_INCLUDED
-#define ENEMY_H_INCLUDED
+// #ifndef ENEMY_H_INCLUDED
+// #define ENEMY_H_INCLUDED
 
-#include "../Object.h"
-#include "../shapes/Rectangle.h"
-#include <vector>
-#include <queue>
+// #include "../Object.h"
+// #include "../shapes/Rectangle.h"
+// #include <vector>
+// #include <queue>
 
-enum class Dir;
+// enum class Dir;
 
-// fixed settings
-enum class EnemyType {
-	GHOST, ENEMYTYPE_MAX
-};
+// // fixed settings
+// enum class EnemyType {
+// 	GHOST, ENEMYTYPE_MAX
+// };
 
-/**
- * @brief The class of a monster (enemies).
- * @details Monster inherits Object and takes Rectangle as its hit box.
- */
-class Enemy : public Object
-{
-public:
-	static Enemy *create_enemy(EnemyType type, const std::vector<Point> &path);
-public:
-	Enemy(const std::vector<Point> &path, EnemyType type);
-	void update();
-	void draw();
-	const std::queue<Point> &get_path() const { return path; }
-protected:
-	/**
-	 * @var HP
-	 * @brief Health point of a monster.
-	 **
-	 * @var v
-	 * @brief Moving speed of a monster.
-	 **
-	 * @var money
-	 * @brief The amount of money that player will earn when the monster is killed.
-	 **
-	 * @var bitmap_img_ids
-	 * @brief The first vector is the Dir index, and the second vector is image id.
-	 * @details `bitmap_img_ids[Dir][<ordered_id>]`
-	 **
-	 * @var bitmap_switch_counter
-	 * @brief Counting down for bitmap_switch_freq.
-	 * @see Monster::bitmap_switch_freq
-	 **
-	 * @var bitmap_switch_freq
-	 * @brief Number of frames required to change to the next move pose for the current facing direction.
-	 * @details The variable is left for child classes to define.
-	 * 
-	 * @var bitmap_img_id
-	 * @brief Move pose of the current facing direction.
-	 **
-	 * @var dir
-	 * @brief Current facing direction.
-	 **
-	 * @var path
-	 * @brief The walk path of a monster, represented in grid format.
-	 * @see Level::grid_to_region(const Point &grid) const
-	*/
-	int v;
-	//std::vector<std::vector<int>> bitmap_img_ids;
-	int bitmap_switch_counter;
-	int bitmap_switch_freq;
-	int bitmap_img_id;
-private:
-	EnemyType type;
-	Dir dir;
-	std::queue<Point> path;
-};
+// /**
+//  * @brief The class of a monster (enemies).
+//  * @details Monster inherits Object and takes Rectangle as its hit box.
+//  */
+// class Enemy : public Object
+// {
+// public:
+// 	static Enemy *create_enemy(EnemyType type, const std::vector<Point> &path);
+// public:
+// 	Enemy(const std::vector<Point> &path, EnemyType type);
+// 	void update();
+// 	void draw();
+// 	const std::queue<Point> &get_path() const { return path; }
+// protected:
+// 	/**
+// 	 * @var HP
+// 	 * @brief Health point of a monster.
+// 	 **
+// 	 * @var v
+// 	 * @brief Moving speed of a monster.
+// 	 **
+// 	 * @var money
+// 	 * @brief The amount of money that player will earn when the monster is killed.
+// 	 **
+// 	 * @var bitmap_img_ids
+// 	 * @brief The first vector is the Dir index, and the second vector is image id.
+// 	 * @details `bitmap_img_ids[Dir][<ordered_id>]`
+// 	 **
+// 	 * @var bitmap_switch_counter
+// 	 * @brief Counting down for bitmap_switch_freq.
+// 	 * @see Monster::bitmap_switch_freq
+// 	 **
+// 	 * @var bitmap_switch_freq
+// 	 * @brief Number of frames required to change to the next move pose for the current facing direction.
+// 	 * @details The variable is left for child classes to define.
+// 	 * 
+// 	 * @var bitmap_img_id
+// 	 * @brief Move pose of the current facing direction.
+// 	 **
+// 	 * @var dir
+// 	 * @brief Current facing direction.
+// 	 **
+// 	 * @var path
+// 	 * @brief The walk path of a monster, represented in grid format.
+// 	 * @see Level::grid_to_region(const Point &grid) const
+// 	*/
+// 	int v;
+// 	//std::vector<std::vector<int>> bitmap_img_ids;
+// 	int bitmap_switch_counter;
+// 	int bitmap_switch_freq;
+// 	int bitmap_img_id;
+// private:
+// 	EnemyType type;
+// 	Dir dir;
+// 	std::queue<Point> path;
+// };
 
-#endif
+// #endif
